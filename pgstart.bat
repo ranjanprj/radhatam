2956fd4ff076a42abaefb51dffdf3f5a4820fac0
ranjanprj <ranjanprj@users.noreply.github.com>
2023-03-17 13:58:57 +0530

    Update pgstart.bat

--- pgstart.bat
+++ pgstart.bat
@@ -7,16 +7,16 @@
 @REM set PATH=%PATH%;C:\3Projects\radhatam\venv\
 
 
-@REM C:\Users\pranjan24\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\pranjan24\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\data -l logfile stop
-@REM C:\Users\pranjan24\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\pranjan24\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\data -l logfile start
+@REM C:\Users\\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\data -l logfile stop
+@REM C:\Users\\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\\Downloads\postgresql-14.2-2-windows-x64-binaries\pgsql\data -l logfile start
 
-C:\Users\pranjan24\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\pranjan24\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\data -l logfile stop
+C:\Users\\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\data -l logfile stop
 
-C:\Users\pranjan24\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\pranjan24\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\data -l logfile start
+C:\Users\\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\bin\pg_ctl -D C:\Users\\Downloads\postgresql-14.6-1-windows-x64-binaries\pgsql\data -l logfile start
 
 
 
-@REM C:\Users\pranjan24\AppData\Local\Programs\Python\Python310\Scripts\pip3.10
+@REM C:\Users\\AppData\Local\Programs\Python\Python310\Scripts\pip3.10
 @REM npx postgraphile -c postgres://postgres:postgres@localhost:5432/postgres?ssl=false --watch
 @REM postgres://postgres:postgres@localhost:5432/data?ssl=false
 
